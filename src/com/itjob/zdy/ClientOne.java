5273e9e7ec6ac6a3dc3038c5fa4a910cf309eb5e
zhongdyyy <2639049628@qq.com>
2018-03-17 10:55:35 +0800

    [新增代码]同步初始化功能代码

---
+++ src/com/itjob/zdy/ClientOne.java
@@ -0,0 +1,42 @@
+package com.itjob.zdy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientOne {
+
+	public static void main(String[] args) {
+		try {
+			Socket  socket = new Socket("127.0.0.1",5200);
+			System.out.println("客户端启动成功");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter pw = new PrintWriter(socket.getOutputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String readline;
+			readline = br.readLine(); // 从系统标准输入读入一字符串
+			while(!readline.equals("end")) {
+				pw.println(readline);
+				pw.flush();
+				System.out.println(in.readLine());
+			}
+			in.close();
+			pw.close();
+			br.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
+
+
